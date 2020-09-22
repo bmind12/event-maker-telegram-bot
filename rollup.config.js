b0d63ad3861c9f0053be65746ef3198c0aa2a461
Sergey <ryabovsergey12@gmail.com>
2020-09-22 04:42:10 +0200

    [issue #1] - align npm main file

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 export default {
     input: 'src/index.ts',
     output: {
-        file: 'dist/bundle.js',
+        file: 'dist/index.js',
         format: 'cjs',
     },
     plugins: [
