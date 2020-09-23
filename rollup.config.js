54296a462c16c3e7abeb6a7699ea86abc6e646f2
Andrei Fidelman <andrei.fidelman@ataccama.com>
2020-09-23 20:40:57 +0200

    [issue #7] - set up the development server

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
+import run from '@rollup/plugin-run';
+import commonJS from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json';
+
+const isDevelopment = process.env.BUILD === 'development'
 
 export default {
     input: 'src/index.ts',
@@ -8,11 +13,17 @@ export default {
         format: 'cjs',
     },
     plugins: [
-        resolve(),
         babel({
             babelHelpers: 'bundled',
             exclude: 'node_modules/**',
             extensions: ['.js', '.ts'],
         }),
+        resolve(),
+        commonJS({
+            include: 'node_modules/**'
+        }),
+        json(),
+        isDevelopment && run()
     ],
+    external: ['express']
 };
