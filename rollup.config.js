754b3ed2ce5ed80ad1e6f17e027746de6798f1c6
Sergey R <ryabovsergey12@gmail.com>
2020-09-23 04:05:10 +0200

    [issue #1] - set up a repo (#6)
    * [issue #1] - set up a repo

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/index.js',
+        format: 'cjs',
+    },
+    plugins: [
+        resolve(),
+        babel({
+            babelHelpers: 'bundled',
+            exclude: 'node_modules/**',
+            extensions: ['.js', '.ts'],
+        }),
+    ],
+};
