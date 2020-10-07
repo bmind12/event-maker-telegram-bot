98ff738561a09428e6a256aefa8646a6c27bd4c9
Sergey <ryabovsergey12@gmail.com>
2020-10-07 03:47:24 +0200

    [issue #5] - add default port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
     res.send('Hello! I am event maker telegram bot');
